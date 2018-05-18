6b301d4cbaf2f4e0f1f23ae35f714b431f80a912
Jayke Peters <37952082+jaykepeters@users.noreply.github.com>
2018-05-17 19:23:12 -0500

    install-chronometer2.sh - v0.1

--- Raspbian/Scripts/install-chronometer2.sh
+++ Raspbian/Scripts/install-chronometer2.sh
@@ -2,7 +2,7 @@
 
 ## Chronometer 2 Installation Script
 ## Initial Commit
-## Version 0.0.0
+## Version 0.1.0
 ## Copyright 2018 Jayke Peters
 
 ## Install Chronometer 2
@@ -19,9 +19,9 @@ chmod +x chronometer2.sh
 # add ./chronometer2.sh to the end of ~/.bashrc
 
 # Configure Sleep Options
-# add this to crontab
-# PiTFT+ SLEEPY TIME
-# Turn off the PiTFT+ at midnight
-00 00 * * * sh -c 'echo "0" > /sys/class/backlight/soc\:backlight/brightness'
-# Turn on the PiTFT+ at 8:00 am
-00 08 * * * sh -c 'echo "1" > /sys/class/backlight/soc\:backlight/brightness'
+crontab -l | { cat; echo ""; } | crontab -
+crontab -l | { cat; echo "# PiTFT+ SLEEPY TIME"; } | crontab -
+crontab -l | { cat; echo "# Turn off the PiTFT+ at midnight"; } | crontab -
+crontab -l | { cat; echo "00 00 * * * sh -c 'echo "0" > /sys/class/backlight/soc\:backlight/brightness'"; } | crontab -
+crontab -l | { cat; echo "# Turn on the PiTFT+ at 8:00 am"; } | crontab -
+crontab -l | { cat; echo "00 08 * * * sh -c 'echo "1" > /sys/class/backlight/soc\:backlight/brightness'"; } | crontab -
